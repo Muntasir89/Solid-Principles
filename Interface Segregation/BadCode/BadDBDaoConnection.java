a0998759e4aaaaeea7913e7fddb88517fb2ce00d
Muntasir Hossen <montachirhossen76@gmail.com>
2023-05-31 12:28:09 +0600

    Interface Segregation added

--- Interface Segregation/BadCode/BadDBDaoConnection.java
+++ Interface Segregation/BadCode/BadDBDaoConnection.java
@@ -1,24 +1,19 @@
 public class BadDBDaoConnection implements BadDAOInterface{
-
     @Override
     public void openConnection() {
         
     }
-
     @Override
     public void createRecord() {
         
     }
-
     @Override
     public void openFile() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'openFile'");
     }
-
     @Override
     public void deleteRecord() {
         
     }
-    
 }
--- Interface Segregation/GoodCode/FileDaoConnection.java
+++ Interface Segregation/GoodCode/FileDaoConnection.java
@@ -1,18 +1,14 @@
 public class FileDaoConnection implements FileInterface, DAOInterface{
-
     @Override
     public void createRecord() {
         
     }
-
     @Override
     public void deleteRecord() {
         
     }
-
     @Override
     public void openFile() {
         
     }
-    
 }
